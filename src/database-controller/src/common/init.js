52deb16d69c33d7f1b8f8598ae1e348df77afe30
Zhiyuan He <zhiyuhe@microsoft.com>
2020-09-29 14:37:33 +0800

    fix

--- src/database-controller/src/common/init.js
+++ src/database-controller/src/common/init.js
@@ -1,5 +1,7 @@
 const logger = require('@dbc/common/logger');
 
+// We doesn't allow unhandled promise rejection.
+// It will make the watcher stop all actions (in some rare cases).
 process.on('unhandledRejection', function(reason, p){
     logger.error(`Encounter unhandled rejection of promise, reason: ${reason}`,
       function() {
